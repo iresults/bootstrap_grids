89dc1d39b0847c14e2c3cfda5cab571c80d0b23f
Daniel Corn <daniel@iresults.li>
2023-02-22 17:47:28 +0100

    Add support for TYPO3 v10 and Grid Elements v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,28 +5,28 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'Grids for bootstrap',
-	'description' => 'Gridelements for bootstrap. Column grids, grids for simple accordions, tabs and content slider.',
-	'category' => 'misc',
-	'author' => 'Pascal Mayer',
-	'author_email' => 'typo3@lascap.ch',
-	'author_company' => '',
-	'version' => '1.4.1',
-	'state' => 'stable',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'constraints' => [
-		'depends' => [
-			'typo3' => '8.7.0-9.5.99',
-			'gridelements' => '8.0.0-9.9.99',
-		],
-		'conflicts' => [
-		],
-	],
-    'autoload' => [
-        'psr-4' => ['Laxap\\BootstrapGrids\\' => 'Classes']
+    'title'            => 'Grids for bootstrap',
+    'description'      => 'Gridelements for bootstrap. Column grids, grids for simple accordions, tabs and content slider.',
+    'category'         => 'misc',
+    'author'           => 'Pascal Mayer',
+    'author_email'     => 'typo3@lascap.ch',
+    'author_company'   => '',
+    'version'          => '1.4.1',
+    'state'            => 'stable',
+    'uploadfolder'     => '0',
+    'createDirs'       => '',
+    'modify_tables'    => '',
+    'clearCacheOnLoad' => 1,
+    'constraints'      => [
+        'depends'   => [
+            'typo3'        => '8.7.0-10.4.99',
+            'gridelements' => '8.0.0-10.9.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload'         => [
+        'psr-4' => ['Laxap\\BootstrapGrids\\' => 'Classes'],
     ],
 ];
 
